08b8235c07c643b0c9fac6418ccd88d113590524
Maotou <93722462+maotou-spy@users.noreply.github.com>
2026-04-02 06:09:32 +0700

    init project

---
+++ playwright.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: 1,
+  reporter: [
+    ["html", { outputFolder: "tests-report", open: "never" }],
+    ["list"],
+  ],
+  use: {
+    baseURL: "https://www.agoda.com",
+    headless: false,
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+    trace: "retain-on-failure",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
